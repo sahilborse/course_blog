3f11d09f748f42bc7f142cc170d02bd580d83cfa
sahilborse18 <sahilborse.909098@gmail.com>
2024-11-08 16:07:49 +0530

    new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 import Image from "next/image";
-
+import { Button } from "@/components/ui/button";
 import { useState, useEffect } from "react";
 
-import { useUser } from '@clerk/nextjs';
-import { useRouter } from 'next/navigation';
+
 export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   
@@ -15,14 +14,6 @@ export default function Home() {
       document.documentElement.classList.remove("dark");
     }
   }, [isDarkMode]);
-  const { isLoaded, user, isSignedIn } = useUser();
-  const router = useRouter();
-
-  if (isLoaded && !isSignedIn) {
-    router.push('/sign-in');
-    return null;
-  }
-  console.log(user);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center  min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] relative">
@@ -40,7 +31,7 @@ export default function Home() {
       </button>
      
 
-      <header className="text-2xl font-bold">Welcome to My App</header>
+      <header className="text-2xl font-bold">Welcome to Course blog</header>
 
       {/* Main content section */}
       <main className="flex flex-col items-center space-y-8">
@@ -53,7 +44,8 @@ export default function Home() {
           className="rounded-lg shadow-lg"
         />
         <p className="text-center text-lg max-w-md">
-          This is a sample application demonstrating the Next.js `Image` component and Tailwind CSS dark mode integration.
+        Stories, Insights, and Everything In Between. 📖✨<br/>
+        Empower Your Future, One Course at a Time. 🚀📚
         </p>
      
       </main>
